3b460b72699d9b21095448a97c863002d2b575fb
Sam Clercky <samclercky@pop-os.localdomain>
2019-09-28 01:13:40 +0200

    New structural changes to website

---
+++ src/build.js
@@ -0,0 +1,20 @@
+const pug = require("pug");
+const fs = require("fs");
+const glob = require("glob");
+const path = require("path");
+
+// clean prev data
+require("./clean");
+
+const compileFuncs = [];
+const files = glob.GlobSync("*.pug").found;
+
+for (let fileName of files) {
+    const output = pug.compileFile(fileName)({});
+    const outFile = path.join(path.dirname(fileName), path.basename(fileName, path.extname(fileName)) + '.html')
+    fs.writeFile(outFile, output, (err) => {
+        if (err) throw err;
+        console.log(`${fileName} has been written.`);
+    })
+}
+
---
+++ src/clean.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+const glob = require("glob");
+
+const FILES = "*.html";
+
+const files = glob.GlobSync(FILES).found;
+
+function remove() {
+    for (let fileName of files) {
+        fs.unlink(fileName, (err) => {
+            if (err) throw err;
+            console.log(`${fileName} cleaned up`);
+        })
+    }
+}
+
+module.exports.remove = remove;
+
+remove();
